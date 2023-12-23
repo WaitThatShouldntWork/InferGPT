7cbc75a6f4b689e668c99d5d74886eebfebee0d5
Chris Booth <65222736+WaitThatShouldntWork@users.noreply.github.com>
2023-12-23 07:00:36 +0000

    Remove env and __pycache__ from tracking

--- env/lib/python3.11/site-packages/langsmith/evaluation/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Evaluation Helpers."""
-
-from langsmith.evaluation.evaluator import EvaluationResult, RunEvaluator, run_evaluator
-from langsmith.evaluation.string_evaluator import StringEvaluator
-
-__all__ = ["run_evaluator", "EvaluationResult", "RunEvaluator", "StringEvaluator"]
