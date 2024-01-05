7695f424d0c22d083f78134ec306ff83040ce5ef
Oliver Cronk <oliver@cronky.net>
2024-01-05 01:07:33 +0000

    Update WidgetContainer.jsx
    Generic UK Retail bank

--- financialhealthcheck/samples/chatscope/example-chat-widget/src/WidgetContainer.jsx
+++ financialhealthcheck/samples/chatscope/example-chat-widget/src/WidgetContainer.jsx
@@ -8,9 +8,9 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 const user = "User";
-const aroc = "Aroc";
+const Bot0 = "Bot0";
 
-export const WidgetContainer = ({greeting = "Hello, I'm Aroc. How can I help you today?"}) => {
+export const WidgetContainer = ({greeting = "Hello, I'm Bot0. How can I help you today?"}) => {
 
     const [messages, setMessages] = useState([]);
 
@@ -19,7 +19,7 @@ export const WidgetContainer = ({greeting = "Hello, I'm Aroc. How can I help you
             setMessages(messages.concat({
                 _id: nanoid(),
                 message: greeting,
-                sender: aroc,
+                sender: Bot0,
                 direction: "incoming",
             }));
         }
@@ -35,14 +35,14 @@ export const WidgetContainer = ({greeting = "Hello, I'm Aroc. How can I help you
         setMessages(messages.concat(newMessages));
 
         const prompt = 
-            "You are a financial advisor AI called Aroc that works for NatWest Bank based solely in the UK.\n" 
+            "You are a financial advisor AI called Bot0 that works for UKRetail Bank based solely in the UK.\n" 
             + "The financial advisor is helpful, polite, creative, clever, and very friendly.\n" 
             + "The financial advisor helps customer solve problems without needing to call agents by collecting information from the customer in order to recommend products.\n"
             + "You must ask at least 7 questions, one at a time, before providing a summary of the customer along with your recommendations.\n"
             + "The conversation goal is a financial health check.\n" 
             + messages.map(m => m.sender + ": " + m.message).join("\n")
             + `\n${user}: ` + message
-            + `\n${aroc}:`;
+            + `\n${Bot0}:`;
         const response = await openai.createCompletion({
             model: "text-davinci-003",      
             prompt: prompt,             
@@ -51,13 +51,13 @@ export const WidgetContainer = ({greeting = "Hello, I'm Aroc. How can I help you
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0.6,
-            stop: [`${user}:`, `${aroc}:`]
+            stop: [`${user}:`, `${Bot0}:`]
         });
         const answer = response.data.choices[0].text.trimStart();
         newMessages.push({
             _id: nanoid(),
             message: answer,
-            sender: aroc,
+            sender: Bot0,
             direction: "incoming",
         });
         setMessages(messages.concat(newMessages));
