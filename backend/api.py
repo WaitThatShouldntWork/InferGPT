24ea23b2855d67d746fbeebaf6e5606bc24aaa04
HélèneSauvé <hsauve@scottlogic.com>
2024-03-27 10:40:03 +0000

    Import and set up FastAPI

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,10 @@
 # TODO: Add basic FastAPI app for communicating with the front end
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
