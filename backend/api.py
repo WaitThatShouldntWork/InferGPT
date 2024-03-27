e973a7bfbf00ba6398bd12cf4ddf5707730cb813
HélèneSauvé <hsauve@scottlogic.com>
2024-03-27 10:48:34 +0000

    Add healthcheck endpoint

--- backend/api.py
+++ backend/api.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/health")
+async def health_check():
+    return {"message": "Infer GPT is running!"}
+
