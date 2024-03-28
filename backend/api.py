96f18b01523827e68bdca40a40344ed014f91e23
Harry <hbedford@scottlogic.com>
2024-03-28 17:06:41 +0000

    WIP - add CORS and hit health endpoint from frontend

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,21 @@
 # TODO: Add basic FastAPI app for communicating with the front end
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8650",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 async def health_check():
