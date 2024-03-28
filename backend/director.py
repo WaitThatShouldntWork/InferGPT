0c4200aed6bf5a5fcfa2a334609c26b7371d8335
Ch0live <colive@scottlogic.com>
2024-03-28 14:43:40 +0000

    Connect FastAPI to mistral-large

--- backend/director.py
+++ backend/director.py
@@ -1,3 +1,18 @@
-# TODO: Add basic director implementation
-def question():
-    return "Hello World!"
+from utils.llm import call_model
+
+prompt= """
+You are a Chat Bot called InferGPT.
+
+You're abilities include:
+
+- Setting and tracking Goals
+- Remembering conversation information from previous sessions and using it in future discussions
+
+Introduce yourself if asked.
+"""
+
+def question(question):
+
+  prompt_with_question = prompt + "\n\nAnswer the following question: " + question
+
+  return call_model(prompt_with_question)
--- backend/utils/llm.py
+++ backend/utils/llm.py
@@ -1,12 +1,32 @@
 import os
 from dotenv import load_dotenv
 from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
 
 load_dotenv()
 
-endpoint = os.environ["INFER_GPT_MISTRAL_LARGE_ENDPOINT"]
-api_key = os.environ["INFER_GPT_MISTRAL_LARGE_KEY"]
-model = "azureai"
+try:
+  endpoint = os.environ["INFER_GPT_MISTRAL_LARGE_ENDPOINT"]
+  api_key = os.environ["INFER_GPT_MISTRAL_LARGE_KEY"]
+  key = os.getenv("OPENAI_API_KEY")
+  model = "azureai" # might me mistral-large
 
-client = MistralClient(api_key=api_key,
-                       endpoint=endpoint)
+  client = MistralClient(api_key=api_key, endpoint=endpoint)
+except FileNotFoundError:
+   raise FileNotFoundError("Please provide a .env file. See the Getting Started guide on the README.md")
+except:
+   raise Exception("Missing .env file property. See the Getting Started guide on the README.md")
+
+def call_model(prompt):
+   response = client.chat(
+      model=model,
+      messages=[
+         ChatMessage(
+            role="user",
+            content=prompt
+         )
+      ],
+      max_tokens=125
+   )
+   print(response.choices[0].message.content)
+   return response.choices[0].message.content
