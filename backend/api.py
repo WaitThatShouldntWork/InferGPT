c88541601905ec9a52a65cec2078120e6b87c22f
ch0live <dozeyis@gmail.com>
2024-04-02 09:53:14 +0100

    WIP - add CORS and hit health endpoint from frontend

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from director import question
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8650",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 async def health_check():
--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -5,6 +5,14 @@ export interface Response {
 export const getResponse = async (message: string): Promise<Response> => {
   // TODO - get response from backend API using env variable for endpoint
   // (.env variables available via process.env)
+  // const response = await fetch(`${process.env.BACKEND_ENDPOINT}/chat?utterance=${message}`, {
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // });
+  const response = await fetch('http://127.0.0.1:8000/health');
+  console.log('HELLO - response: ', response.json());
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({ message });
