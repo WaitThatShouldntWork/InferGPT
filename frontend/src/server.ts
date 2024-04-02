e8d14e53ffc50a2dc511a659a5bcf040785c1c39
ch0live <dozeyis@gmail.com>
2024-04-02 10:51:58 +0100

    Add working happy/unhappy healthcheck backend calls

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -2,20 +2,34 @@ export interface Response {
   message: string;
 }
 
+let happyResponse: Response = {
+    message: "InferGPT backend is healthy!"
+}
+
+let unhappyResponse: Response = {
+    message: "Error: InferGPT backend is unhealthy"
+}
+
+let unsupportedResponse: Response = {
+    message: "Error: General chat response unsupported"
+}
+
 export const getResponse = async (message: string): Promise<Response> => {
-  // TODO - get response from backend API using env variable for endpoint
-  // (.env variables available via process.env)
-  // const response = await fetch(`${process.env.BACKEND_ENDPOINT}/chat?utterance=${message}`, {
-  //   method: 'GET',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // });
-  const response = await fetch(`${process.env.BACKEND_ENDPOINT}/health`);
-  console.log('HELLO - response: ', response.json());
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ message });
-    }, 1000);
-  });
+    if (message == "healthcheck") {
+        try {
+            const response = await fetch(`${process.env.BACKEND_ENDPOINT}/health`);
+            console.log('InferGPT backend is healthy!: ', response.json());
+            return new Promise((resolve) => {
+                setTimeout(() => {resolve(happyResponse);}, 1000);
+            });
+        } catch {
+            return new Promise((resolve) => {
+                setTimeout(() => {resolve(unhappyResponse);}, 1000);
+            })
+        }
+    } else {
+        return new Promise((resolve) => {
+            setTimeout(() => {resolve(unsupportedResponse);}, 1000);
+        })
+    }
 };
