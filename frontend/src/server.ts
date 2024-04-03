b8e7bfbc893f79b775a48e98aa94fcc588ecfdf5
Ch0live <dozeyis@gmail.com>
2024-04-03 09:28:08 +0100

    Wire up InferGPT frontend and backend (#17)
    * WIP - add CORS and hit health endpoint from frontend
    * Use backend url from .env
    * Add working happy/unhappy healthcheck backend calls
    * Add working /chat connection from frontend
    * Tweak of system prompt to allow conversation

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -1,13 +1,50 @@
-export interface Response {
+export interface ChatMessageResponse {
   message: string;
 }
 
-export const getResponse = async (message: string): Promise<Response> => {
-  // TODO - get response from backend API using env variable for endpoint
-  // (.env variables available via process.env)
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ message });
-    }, 3000);
-  });
+const happyResponse: ChatMessageResponse = {
+  message: 'InferGPT backend is healthy!'
+};
+
+const unhappyResponse: ChatMessageResponse = {
+  message: 'Error: InferGPT backend is unhealthy'
+};
+
+export const getResponse = async (message: string): Promise<ChatMessageResponse> => {
+  if (message == 'healthcheck') {
+    return checkBackendHealth();
+  } else {
+    return callChatEndpoint(message);
+  }
+};
+
+const checkBackendHealth = async (): Promise<ChatMessageResponse> => {
+  try {
+    const response = await fetch(`${process.env.BACKEND_ENDPOINT}/health`);
+    console.log('InferGPT backend is healthy!: ', response);
+    return happyResponse;
+  } catch {
+    return unhappyResponse;
+  }
+};
+
+const callChatEndpoint = async (message: string): Promise<ChatMessageResponse> => {
+  return await fetch(`${process.env.BACKEND_ENDPOINT}/chat?utterance=${message}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response;
+    })
+    .then(response => response.json())
+    .then(responseJson => {
+      const responseAsChatMessageResponse: ChatMessageResponse = {
+        message: responseJson
+      };
+      return responseAsChatMessageResponse;
+    })
+    .catch(error => {
+      console.error('Error making REST call to /chat: ', error);
+      return unhappyResponse;
+    });
 };
