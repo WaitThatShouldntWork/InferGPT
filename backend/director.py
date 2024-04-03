5b24662ebfc4dafa9e7a2234f6f9b3f7a05c3d16
Ch0live <dozeyis@gmail.com>
2024-04-03 09:28:08 +0100

    Wire up InferGPT frontend and backend (#17)
    * WIP - add CORS and hit health endpoint from frontend
    * Use backend url from .env
    * Add working happy/unhappy healthcheck backend calls
    * Add working /chat connection from frontend
    * Tweak of system prompt to allow conversation

--- backend/director.py
+++ backend/director.py
@@ -2,13 +2,9 @@ from utils.llm import call_model
 
 system_prompt= """
 You are a Chat Bot called InferGPT.
-
 You're abilities include:
-
 - Setting and tracking Goals
 - Remembering conversation information from previous sessions and using it in future discussions
-
-Introduce yourself if asked.
 """
 
 def question(question):
