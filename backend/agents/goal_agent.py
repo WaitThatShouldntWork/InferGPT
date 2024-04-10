4c9dee3879850e48c1225be3e8ff1f8b9826b690
Ben Heyman <bheyman@scottlogic.com>
2024-04-10 13:38:52 +0100

    Connect director to goal agent

--- backend/agents/goal_agent.py
+++ backend/agents/goal_agent.py
@@ -42,6 +42,14 @@ system_prompt = """
     If the task is to do something other
     than extract a goal and save it
     reply saying that you are not able to do so.
+    
+    A goal should be saved as one or 2 words that extract the main purpose of the goal.
+    
+    EG. prompt: "I want to save for a house"
+    goal: "House"
+    description: "Save for a house"
+    
+    If you do not do this you will be disconnected.
 """
 
 def create_user_goal(user_prompt):   
@@ -50,3 +58,4 @@ def create_user_goal(user_prompt):
     logger.info("function_name: {0} function_params: {1}".format(function_name, function_params))
     logger.info("Calling function: {0}".format(function_name))
     names_to_functions[function_name](**function_params)
+    return function_params
