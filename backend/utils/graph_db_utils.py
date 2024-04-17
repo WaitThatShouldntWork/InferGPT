d10f813594d3cb387e19595110b0eadbcb453c42
Ch0live <dozeyis@gmail.com>
2024-04-17 16:03:26 +0100

    Dockerize app & improve healthcheck logic (#28)
    * Add docker-compose
    * Rename frontend .env property
    * Mention the need to create .env files
    * Add Neo4J guidance
    * Add better healthcheck logic
    * Fix linting issue

--- backend/utils/graph_db_utils.py
+++ backend/utils/graph_db_utils.py
@@ -10,16 +10,20 @@ AUTH = (config.neo4j_user, config.neo4j_password)
 driver = GraphDatabase.driver(URI, auth=AUTH)
 
 def test_connection():
+    logging.info("testing database connection...")
+    connectionHealthy = False;
     try:
         driver.verify_connectivity()
-        logging.debug("database connection active")
+        logging.info("database connection verified")
+        connectionHealthy = True;
         
     except Exception as e:
-        logging.exception(f"Error: {e}")
-        raise
+        logging.critical("connection connection failed")
+        logging.critical(e)
         
     finally:
         driver.close()
+        return connectionHealthy;
     
 def create_goal(name, description):
     try:
