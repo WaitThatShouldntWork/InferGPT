aed6eb0c42a70475f55360674ca4d9a59e1a801a
ch0live <dozeyis@gmail.com>
2024-04-18 13:42:21 +0100

    Fix backend linting issues

--- backend/api.py
+++ backend/api.py
@@ -3,7 +3,7 @@ import logging.config
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from utils import Config
+from utils import Config, test_connection
 from director import question
 
 logging.config.fileConfig(fname="config.ini", disable_existing_loggers=False)
@@ -24,18 +24,22 @@ app.add_middleware(
 
 healthy_response = {"message": "InferGPT backend is healthy"}
 error_message = "Unable to formulate InferGPT response"
+health_prefix = "InferGPT healthcheck: "
+unhealthy_neo4j_response = "unhealthy. Please check the README files for further guidance"
 
 
 @app.get("/health")
 async def health_check():
     try:
         logger.info("health_check method called successfully")
-        neo4j_status = "healthy" if test_connection() == True else "unhealthy. Please check the README files for further guidance"
+        neo4j_status = "healthy" if test_connection() else unhealthy_neo4j_response
         logging.info("health_check method complete")
-        return JSONResponse(status_code=200, content="InferGPT healthcheck: backend is healthy. Neo4J is " + neo4j_status)
+        return JSONResponse(status_code=200, content=health_prefix + "backend is healthy. Neo4J is " + neo4j_status)
     except Exception as e:
         logger.critical("health_check method failed with error: " + e)
-        return JSONResponse(status_code=500, content="InferGPT healthcheck: backend is unhealthy. Unable to healthcheck Neo4J. Please check the README files for further guidance")
+        return JSONResponse(status_code=500, content=health_prefix +
+            "backend is unhealthy. Unable to healthcheck Neo4J. Please check the README files for further guidance"
+        )
 
 @app.get("/chat")
 async def chat(utterance: str):
