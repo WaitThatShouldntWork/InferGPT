e1df50cf7b25c14167ba6141aa2eddc42d305769
HélèneSauvé <hsauve@scottlogic.com>
2024-04-23 13:46:24 +0100

    Change variable names and add logging

--- backend/director.py
+++ backend/director.py
@@ -1,10 +1,10 @@
 import logging
 from utils.llm import call_model
-from create_tasks_agent import create_tasks
+from agents.create_tasks_agent import create_tasks
 
 logger = logging.getLogger(__name__)
 
-system_prompt = """
+director_prompt = """
 You are a Chat Bot called InferGPT.
 Your sole purpose is to get the user to tell you their intention. The intention has to be specifically related
 to the user. For example:
@@ -27,7 +27,7 @@ How many grams are there in an ounce?
 
 """
 
-system_prompt_to_determine_intent = """
+determine_intention_prompt = """
 Your purpose is to determine whether the user prompt contains an intention that can be broken down into smaller tasks.
 If the user prompt contains an intention return "TRUE". If it does not contain an intention return "FALSE".
 
@@ -42,18 +42,15 @@ If you reply more than one word, you will be disconnected
 
 
 def question(question):
-    has_intent = determine_intent(question) == "TRUE"
-    print(has_intent)
+    has_intent = determine_intention(question) == "TRUE"
+    logger.info(f"intention identified: {has_intent}")
     if has_intent:
         return create_tasks(question)
 
     logger.info("director calling call_model function")
-    return call_model(system_prompt, user_prompt=question)
+    return call_model(director_prompt, user_prompt=question)
 
 
-def determine_intent(question):
-    logger.info("director calling determine_intent function")
-    return call_model(system_prompt_to_determine_intent, user_prompt=question)
-
-
-print(question("What did I spend more on last month, Amazon or Netflix?"))
+def determine_intention(question):
+    logger.info("director calling determine_intention function")
+    return call_model(determine_intention_prompt, user_prompt=question)
