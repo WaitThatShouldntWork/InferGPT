c3622d21241249e15a18b41fad0456cce0b7ef6b
HélèneSauvé <hsauve@scottlogic.com>
2024-04-23 11:52:23 +0100

    Edit Director to return tasks

--- backend/create_tasks_agent.py
+++ backend/create_tasks_agent.py
@@ -1,41 +1,41 @@
 from utils import Config
 from utils import call_model
+import logging
 
+logger = logging.getLogger(__name__)
 config = Config()
 
 system_prompt = """
-    You will be provided with a goal provided by the user.
-    Your role is to breakdown the goal into up to 5 tasks that would be requiredl to accomplish this goal
+    You are an agent who specialises in breaking down questions into smaller tasks that are performed to complete
+    the question.
 
-    EG. prompt: "Which bank should I choose to open a savings account based on the best interest rate between Lloyds Bank and Tesco Bank?"
-    1) Find the interest rate for Lloyds Bank's savings account
-    2) Find the interest rate for Tesco Bank's savings account
-    3) Compare both values
-"""
+    You are dedicated to performing the job of breaking down a user question into up to 3 tasks that are easy
+    to understand and manageable in size.
 
-system_prompt_2 = """
-    You are an agent who specialises in breaking down questions into smaller tasks that are performed to complete the question.
-    
-    You are dedicated to performing the job of breaking down a user question into up to 5 tasks that are easy to understand and manageable in size. 
-    A user will provide a goal that they want to perform and you will create tasks that when all of them are performed, will provide an answer for the user question.
-    
-    To determine the tasks you will create, you will use reasoning, any context you have and  will attempt to criticise each task in order to find the best way possible for tasks to answer the overall question.
-    
-    You are connected to a neo4j database, as a source of information. You do not need to create a task to connect or retrieve user data from the database. 
-    
-    EG. prompt: "Which bank should I choose to open a savings account based on the best interest rate between Lloyds Bank and Tesco Bank?"
-    1) **Find the interest rate for Lloyds Bank's savings account**: provide an explanation for your reasoning
-    2) **Find the interest rate for Tesco Bank's savings account**: provide an explanation for your reasoning
-    3) **Compare both values**: provide an explanation for your reasoning
+    A user will provide a goal that they want to perform and you will create tasks that when all of them are performed,
+    will provide an answer for the user question.
+
+    To determine the tasks you will create, you will use reasoning, any context you have and will attempt to criticise
+    each task in order to find the best way possible for tasks to answer the overall question.
+
+    If you are unable to breakdown the question into tasks, say that unfortunately you are unable to break down
+    the question.
+
+    You are connected to a neo4j database, as a source of information. You do not need to create a task to connect
+    or retrieve user data from the database.
+
+    EG. prompt: "Which bank should I choose to open a savings account based on the best interest rate
+    between Lloyds Bank and Tesco Bank?"
+    1) Find the interest rate for Lloyds Bank's savings account. Explanation: {your reasoning}
+    2) Find the interest rate for Tesco Bank's savings account. Explanation: {your reasoning}
+    3) Compare both values.
 
     EG. prompt: "How much did I spend last month on my debit card?"
-    1) **Find all spending transactions last month on the user's debit card**: provide an explanation for your reasoning
-    2) **Sum all transactions**: provide an explanation for your reasoning
+    1) Find all spending transactions last month on the user's debit card. Explanation: {your reasoning}
+    2) Sum all transactions. Explanation: {your reasoning}
 """
 
 def create_tasks(user_prompt):
-    response = call_model(system_prompt_2, user_prompt)
-    print(response)
+    logger.info("create_tasks function is called")
+    response = call_model(system_prompt, user_prompt)
     return(response)
-
-create_tasks("How can I save money for a house?")
--- backend/director.py
+++ backend/director.py
@@ -1,18 +1,35 @@
 import logging
 from utils.llm import call_model
-from agents.goal_agent import create_user_goal
+from create_tasks_agent import create_tasks
 
 logger = logging.getLogger(__name__)
 
 system_prompt = """
 You are a Chat Bot called InferGPT.
-Your sole purpose is to get the user to tell you their goal.
-If the user does not provide a goal, ask them to provide a goal
+Your sole purpose is to get the user to tell you their intention. The intention has to be specifically related
+to the user. For example:
+-spending
+-personal interests
+
+If the user does not provide an intention or the intention isn't directly related to the user,
+attempt to provide an answer.
+If you do not know the answer, do not try to make it up.
+
+Eg.
+An intention:
+I want to save for a house
+How much did I spend last month?
+
+Not an intention:
+How are you?
+How many grams are there in an ounce?
+
+
 """
 
 system_prompt_to_determine_intent = """
-Your purpose is to determine whether the user prompt contains a goal.
-If the user prompt contains a goal return "TRUE". If it does not contain a goal return "FALSE".
+Your purpose is to determine whether the user prompt contains an intention that can be broken down into smaller tasks.
+If the user prompt contains an intention return "TRUE". If it does not contain an intention return "FALSE".
 
 Your reply should be one word only:
 If goal:
@@ -25,17 +42,18 @@ If you reply more than one word, you will be disconnected
 
 
 def question(question):
-    logger.info("director calling call_model method")
     has_intent = determine_intent(question) == "TRUE"
-
+    print(has_intent)
     if has_intent:
-        goal_saved = create_user_goal(question)
-        return "I have created a goal for you. Goal: {0}. Description: {1}".format(
-            goal_saved["name"], goal_saved["description"]
-        )
+        return create_tasks(question)
 
+    logger.info("director calling call_model function")
     return call_model(system_prompt, user_prompt=question)
 
 
 def determine_intent(question):
+    logger.info("director calling determine_intent function")
     return call_model(system_prompt_to_determine_intent, user_prompt=question)
+
+
+print(question("What did I spend more on last month, Amazon or Netflix?"))
