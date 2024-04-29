7ed753d5cd6ae509bf739a3d90d19afbf2eb4b9c
Danny Nicholas <dannynicholas23@gmail.com>
2024-04-29 11:32:40 +0100

    Fix linting

--- backend/utils/graph_db_utils_test.py
+++ backend/utils/graph_db_utils_test.py
@@ -6,21 +6,21 @@ import utils.graph_db_utils
 def test_database_connectivity_is_healthy(mocker):
     mock_driver = mocker.patch("utils.graph_db_utils.driver", return_value=MagicMock(spec=Driver))
     mock_driver.verify_connectivity.return_value = None
-    
+
     connectivity = utils.graph_db_utils.test_connection()
-    
-    assert connectivity == True
+
+    assert connectivity
     mock_driver.verify_connectivity.assert_called_once()
     mock_driver.close.assert_called_once()
-   
- 
+
+
 def test_database_connectivity_is_unhealthy(mocker):
     mock_driver = mocker.patch("utils.graph_db_utils.driver", return_value=MagicMock(spec=Driver))
     mock_driver.verify_connectivity.side_effect = Exception
-    
+
     connectivity = utils.graph_db_utils.test_connection()
-    
-    assert connectivity == False
+
+    assert not connectivity
     mock_driver.verify_connectivity.assert_called_once()
     mock_driver.close.assert_called_once()
 
@@ -29,24 +29,24 @@ def test_create_goal_is_successful(mocker):
     mock_driver = mocker.patch("utils.graph_db_utils.driver", return_value=MagicMock(spec=Driver))
     mock_session = MagicMock(spec=Session)
     mock_driver.session.return_value = mock_session
-    
+
     response = utils.graph_db_utils.create_goal("Test Name", "Test Description")
-    
-    assert response == None
+
+    assert response is None
     mock_session.run.assert_called_once_with(ANY, name="Test Name", description="Test Description")
     mock_session.close.assert_called_once()
     mock_driver.close.assert_called_once()
-    
-    
+
+
 def test_create_goal_throws_exception(mocker):
     mock_driver = mocker.patch("utils.graph_db_utils.driver", return_value=MagicMock(spec=Driver))
     mock_session = MagicMock(spec=Session)
     mock_driver.session.return_value = mock_session
     mock_session.run.side_effect = Exception
-    
+
     with pytest.raises(Exception):
         utils.graph_db_utils.create_goal("Test Name", "Test Description")
-    
+
     mock_session.run.assert_called_once_with(ANY, name="Test Name", description="Test Description")
     mock_session.close.assert_called_once()
     mock_driver.close.assert_called_once()
