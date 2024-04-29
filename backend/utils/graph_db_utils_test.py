ae13c7a85e2d281c21c6f92f93811f382ecd7a46
Danny Nicholas <dannynicholas23@gmail.com>
2024-04-29 17:07:41 +0100

    assign alias to test_connection

--- backend/utils/graph_db_utils_test.py
+++ backend/utils/graph_db_utils_test.py
@@ -1,13 +1,14 @@
 from unittest.mock import ANY, MagicMock
 from neo4j import Driver, Session
 import pytest
-import utils.graph_db_utils
+# We assign an alias to "test_connection" to avoid pytest treating it as another test function
+from utils.graph_db_utils import test_connection as verify_connection, create_goal
 
 def test_database_connectivity_is_healthy(mocker):
     mock_driver = mocker.patch("utils.graph_db_utils.driver", return_value=MagicMock(spec=Driver))
     mock_driver.verify_connectivity.return_value = None
 
-    connected = utils.graph_db_utils.test_connection()
+    connected = verify_connection()
 
     assert connected
     mock_driver.verify_connectivity.assert_called_once()
@@ -18,7 +19,7 @@ def test_database_connectivity_is_unhealthy(mocker):
     mock_driver = mocker.patch("utils.graph_db_utils.driver", return_value=MagicMock(spec=Driver))
     mock_driver.verify_connectivity.side_effect = Exception
 
-    connected = utils.graph_db_utils.test_connection()
+    connected = verify_connection()
 
     assert not connected
     mock_driver.verify_connectivity.assert_called_once()
@@ -30,7 +31,7 @@ def test_create_goal_is_successful(mocker):
     mock_session = MagicMock(spec=Session)
     mock_driver.session.return_value = mock_session
 
-    response = utils.graph_db_utils.create_goal("Test Name", "Test Description")
+    response = create_goal("Test Name", "Test Description")
 
     assert response is None
     mock_session.run.assert_called_once_with(ANY, name="Test Name", description="Test Description")
@@ -45,7 +46,7 @@ def test_create_goal_throws_exception(mocker):
     mock_session.run.side_effect = Exception
 
     with pytest.raises(Exception):
-        utils.graph_db_utils.create_goal("Test Name", "Test Description")
+        create_goal("Test Name", "Test Description")
 
     mock_session.run.assert_called_once_with(ANY, name="Test Name", description="Test Description")
     mock_session.close.assert_called_once()
