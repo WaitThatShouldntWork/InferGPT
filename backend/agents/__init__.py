c223dfc5e24a4229de7c4fd4ed901d29d71d6aa0
Harry <hbedford@scottlogic.com>
2024-04-30 15:38:15 +0100

    remove duplicate import

--- backend/agents/__init__.py
+++ backend/agents/__init__.py
@@ -1,7 +1,6 @@
 from .create_tasks_agent import create_tasks
 from .goal_agent import create_user_goal
 from .tool import tool_metadata
-from .tool import tool_metadata
 from .agent import Agent, agent_metadata
 
 __all__ = ["Agent", "agent_metadata", "create_tasks", "tool_metadata", "create_user_goal"]
