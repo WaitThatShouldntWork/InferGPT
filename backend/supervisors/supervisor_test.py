7271c07ce36206438db980258aca0bfd5ad10fe6
ch0live <dozeyis@gmail.com>
2024-04-30 10:21:23 +0100

    Fix tests

--- backend/supervisors/supervisor_test.py
+++ backend/supervisors/supervisor_test.py
@@ -1,10 +1,22 @@
+import json
 from supervisors import solve_all_tasks
 
-hair_task = "I want to save to die my hair blue, again"
+hair_task = """
+{
+    "tasks": [
+        {
+            "number": 1,
+            "summary": "Find a hair salon near you",
+            "explanation": "To dye your hair we need to find somewhere that offers hair dyes"
+        }
+    ]
+}
+"""
 
 # TODO: Mock out LLM call
 def test_load_task_step_template():
     try:
-        solve_all_tasks(hair_task)
+
+        solve_all_tasks(json.loads(hair_task))
     except Exception:
         raise
