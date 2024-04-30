3734f13dc9cd73135d5f87e81c33ffc7774c6358
Harry <hbedford@scottlogic.com>
2024-04-30 14:17:27 +0100

    lint

--- backend/agents/__init__.py
+++ backend/agents/__init__.py
@@ -1,7 +1,18 @@
 from .create_tasks_agent import create_tasks
+from .goal_agent import create_user_goal
 from .tool import tool_metadata, Parameter, Tool
 from .adapters import get_mistral_tool
 from .types import Action_and_args
 from .agent import Agent, agent_metadata
 
-__all__ = ["Agent", "agent_metadata", "create_tasks", "tool_metadata", "get_mistral_tool", "Parameter", "Action_and_args", "Tool", "create_user_goal"]
+__all__ = [
+    "Agent",
+    "agent_metadata",
+    "create_tasks",
+    "tool_metadata",
+    "get_mistral_tool",
+    "Parameter",
+    "Action_and_args",
+    "Tool",
+    "create_user_goal"
+]
