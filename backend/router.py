da66a060b423dcc801cb78a50fba335aa29e870f
ch0live <dozeyis@gmail.com>
2024-05-01 11:29:46 +0100

    Improve next best step prompt

--- backend/router.py
+++ backend/router.py
@@ -1,7 +1,7 @@
 import json
 import logging
-from utils import get_response_three_prompts
-from prompts.prompting import PromptEngine  # TODO: remove need for .prompting
+from utils import call_model
+from prompts import PromptEngine
 
 # TODO: Create pick_agent test with mocked calls
 prompt_engine = PromptEngine()
@@ -13,23 +13,24 @@ def convert_to_json(next_step):
     except Exception:
         raise Exception("Failed to interpret LLM next step format")
 
-def pick_agent(task_string):
-    logging.debug("Picking agent for task: " + task_string)
+def pick_agent(current_task_string, next_task_string):
+    logging.debug("Picking agent")
 
-    list_of_agents = ["unresolvable_agent, database_agent, financial_advisor_agent, web_search_agent"]
-
-    agent_list_prompt = prompt_engine.load_prompt("agents-list", list_of_agents=list_of_agents)
+    list_of_agents = ["database_agent, financial_advisor_agent, web_search_agent"]
+    best_next_step_prompt = prompt_engine.load_prompt(
+        "best-next-step",
+        current_task=current_task_string,
+        next_task=next_task_string,
+        list_of_agents=list_of_agents
+    )
     response_format_prompt = prompt_engine.load_prompt("agent-selection-format")
 
-    # TODO: Add previous successful step information (e.g. data retrieved from the agents)
-    best_next_step_prompt = prompt_engine.load_prompt("task-step", task=task_string)
+    best_next_step = call_model(response_format_prompt, best_next_step_prompt)
 
-    # while 
+    next_step_json = convert_to_json(best_next_step)
 
-    next_step = get_response_three_prompts(agent_list_prompt, response_format_prompt, best_next_step_prompt)
+    logging.info("For iteration:\n" + best_next_step_prompt)
+    logging.info("Found next best step:")
+    logging.info(next_step_json["thoughts"])
 
-    logging.debug("Found next best step:")
-    next_step_json = convert_to_json(next_step)
-    logging.debug(next_step_json["thoughts"])
-
-    return next_step_json["agent"]
+    return next_step_json
