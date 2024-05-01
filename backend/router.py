0f23c955812f52cafb1f1529d4dcac7371f008f9
ch0live <dozeyis@gmail.com>
2024-05-01 09:07:34 +0100

    Create new looping router prompt

--- backend/router.py
+++ backend/router.py
@@ -7,6 +7,12 @@ from prompts.prompting import PromptEngine  # TODO: remove need for .prompting
 prompt_engine = PromptEngine()
 
 
+def convert_to_json(next_step):
+    try:
+        return json.loads(next_step)
+    except Exception:
+        raise Exception("Failed to interpret LLM next step format")
+
 def pick_agent(task_string):
     logging.debug("Picking agent for task: " + task_string)
 
@@ -14,22 +20,16 @@ def pick_agent(task_string):
 
     agent_list_prompt = prompt_engine.load_prompt("agents-list", list_of_agents=list_of_agents)
     response_format_prompt = prompt_engine.load_prompt("agent-selection-format")
+
+    # TODO: Add previous successful step information (e.g. data retrieved from the agents)
     best_next_step_prompt = prompt_engine.load_prompt("task-step", task=task_string)
+
+    # while 
 
     next_step = get_response_three_prompts(agent_list_prompt, response_format_prompt, best_next_step_prompt)
 
-    # collect all required prompts
-    # system-prompt: List of agents that can be called
-    # system-prompt: Expected response shape (probably a json)
-    # user-prompt: "pick an agent and fit to the response shape to solve {taskString}
-
     logging.debug("Found next best step:")
-
-    try:
-        next_step_json = json.loads(next_step)
-    except Exception:
-        raise Exception("Failed to interpret LLM next step format")
-
+    next_step_json = convert_to_json(next_step)
     logging.debug(next_step_json["thoughts"])
 
     return next_step_json["agent"]
