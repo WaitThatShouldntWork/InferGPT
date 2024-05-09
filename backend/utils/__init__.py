e7b08e7c78f986c5d4536a0296f56af2d141304f
Phil Baxter <phil.baxter@cyclone-technology.com>
2024-05-09 12:07:20 +0100

    remove unused files

--- backend/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import Config
-from .llm import call_model, call_model_with_tools
-from .graph_db_utils import create_goal, test_connection
-
-__all__ = ["Config", "call_model", "call_model_with_tools", "create_goal", "test_connection"]
