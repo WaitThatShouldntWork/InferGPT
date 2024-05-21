85ba704b29cb7791671c9f504013d46ebc94ad64
Harry <hbedford@scottlogic.com>
2024-05-21 11:29:12 +0100

    move and fix api tests

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,3 +1,3 @@
-from .api import app
+from .api import app, healthy_response, unhealthy_neo4j_response, chat_fail_response
 
-__all__ = ["app"]
+__all__ = ["app", "healthy_response", "unhealthy_neo4j_response", "chat_fail_response"]
