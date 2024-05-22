fae9f7f034b928eb6fd7e70897766152b4944ce6
Harry <hbedford@scottlogic.com>
2024-05-22 11:13:19 +0100

    review feedback

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,3 +1,3 @@
-from .api import app, healthy_response, unhealthy_neo4j_response, chat_fail_response
+from .app import app, healthy_response, unhealthy_neo4j_response, chat_fail_response
 
 __all__ = ["app", "healthy_response", "unhealthy_neo4j_response", "chat_fail_response"]
--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,21 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import styles from './app.module.css';
 import { Chat } from './components/chat';
 import { Input } from './components/input';
 import { useMessages } from './useMessages';
-import { ConnectionStatus } from './components/connection-status';
-import { WebsocketContext } from './session/websocket-context';
 
 export const App = () => {
   const { sendMessage, messages, waiting } = useMessages();
-
-  const { isConnected } = useContext(WebsocketContext);
 
   return (
     <div className={styles.container}>
       <Chat messages={messages} waiting={waiting} />
       <Input sendMessage={sendMessage} />
-      <ConnectionStatus isConnected={isConnected} />
     </div>  
   );
 };
