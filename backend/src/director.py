42738be6bf725ca86541e67eb904e8135baee3cf
Ben Heyman <bheyman@scottlogic.com>
2024-06-05 16:31:45 +0100

    Update director

--- backend/src/director.py
+++ backend/src/director.py
@@ -1,9 +1,8 @@
 import json
 import logging
-from src.agents import intent_agent
+from src.agents import intent_agent, answer_agent
 from src.prompts import PromptEngine
 from src.utils import call_model
-from src.agents import create_tasks, agents_details
 from src.supervisors import solve_all_tasks
 
 logging = logging.getLogger(__name__)
@@ -18,11 +17,10 @@ def question(question):
     intent = intent_agent.invoke(question)
     intent_json = json.loads(intent)
     logging.info(f"Intent determined: {intent}")
-    final_answer = solve_all_tasks(intent_json)
+    final_scratchpad = solve_all_tasks(intent_json)
+    final_answer = answer_agent.invoke(question, final_scratchpad)
+    logging.info(f"final answer: {final_answer}")
     return final_answer
-
-    # logging.info("Passing utterance straight to call_model function")
-    # return call_model(director_prompt, user_prompt=question)
 
 
 def determine_intention(question: str) -> str:
