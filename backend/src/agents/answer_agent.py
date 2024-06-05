f0c4f630a3aacfc66657d8cbd1a2fe906bd904d5
Ben Heyman <bheyman@scottlogic.com>
2024-06-05 16:32:29 +0100

    Create AnswerAgent

---
+++ backend/src/agents/answer_agent.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import Optional
+from src.utils import call_model
+from src.prompts import PromptEngine
+from src.agents import Agent, agent
+
+engine = PromptEngine()
+intent_format = engine.load_prompt("intent-format")
+
+datetime = datetime.now()
+
+
+@agent(
+    name="AnswerAgent",
+    description="This agent is responsible for generating an answer for the user, based on results in the scratchpad",
+    tools=[],
+)
+class AnswerAgent(Agent):
+    def invoke(self, question: str, final_scratchpad: Optional[str] = None) -> str:
+        create_answer = engine.load_prompt("create_answer", final_scratchpad=final_scratchpad, datetime=datetime)
+
+        return call_model(create_answer, user_prompt=question)
