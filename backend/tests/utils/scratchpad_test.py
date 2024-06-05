092296d2447eb99943e6cf2be578d750749efe64
Ben Heyman <bheyman@scottlogic.com>
2024-06-05 14:56:45 +0100

    Update scratchpad

--- backend/tests/utils/scratchpad_test.py
+++ backend/tests/utils/scratchpad_test.py
@@ -1,14 +1,23 @@
 from src.utils.scratchpad import clear_scratchpad, get_scratchpad, update_scratchpad
 
 
-task = {
-    'summary': 'example task',
-    'explanation': 'This is an example task'
+question = {
+    "query": "example question",
+    "question_intent": "example intent",
+    "operation": "example operation",
+    "question_category": "example category",
+    "parameters": [{"type": "example type", "value": "example value"}],
+    "aggregation": "none",
+    "sort_order": "none",
+    "timeframe": "none",
 }
+
 
 def test_scratchpad():
     clear_scratchpad()
     assert get_scratchpad() == []
-    update_scratchpad("ExampleAgent", task, "example result")
-    assert get_scratchpad() == [{'agent_name': 'ExampleAgent', 'task': 'example task', 'result': 'example result'}]
+    update_scratchpad("ExampleAgent", question, "example result")
+    assert get_scratchpad() == [
+        {"agent_name": "ExampleAgent", "question": "example question", "result": "example result"}
+    ]
     clear_scratchpad()
