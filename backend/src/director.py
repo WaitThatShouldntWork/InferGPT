e96bba0574ab452378b03da3333b14d78f0c32b1
Ben Heyman <bheyman@scottlogic.com>
2024-06-07 16:29:40 +0100

    Update tests

--- backend/src/director.py
+++ backend/src/director.py
@@ -3,14 +3,12 @@ import logging
 from src.utils.scratchpad import clear_scratchpad
 from src.agents import intent_agent, answer_agent
 from src.prompts import PromptEngine
-from src.utils import call_model
 from src.supervisors import solve_all
 
 logging = logging.getLogger(__name__)
 
 engine = PromptEngine()
 director_prompt = engine.load_prompt("director")
-determine_intention_prompt = engine.load_prompt("determine-intention")
 
 
 def question(question):
@@ -27,8 +25,3 @@ def question(question):
     logging.info("scratchpad cleared")
 
     return final_answer
-
-
-def determine_intention(question: str) -> str:
-    logging.debug("director calling determine_intention function")
-    return call_model(determine_intention_prompt, user_prompt=question)
--- backend/tests/supervisors/supervisor_test.py
+++ backend/tests/supervisors/supervisor_test.py
@@ -1,26 +1,41 @@
 from tests.agents import MockAgent
-from src.supervisors import solve_all_tasks, solve_task, no_tasks_response, unsolvable_response, no_agent_response
+from src.supervisors import solve_all, solve_task, no_tasks_response, unsolvable_response, no_agent_response
 
 mock_answer = "answer"
 scratchpad = []
-task = {"summary": "Solve this problem"}
-task_dict = {"tasks": [task]}
+task = {"query": "Solve this problem"}
+intent_json = {
+    "query": "example query?",
+    "user_intent": "example intent",
+    "questions": [
+        {
+            "query": "example query",
+            "question_intent": "example intent",
+            "operation": "example operation",
+            "question_category": "example category",
+            "parameters": [{"type": "example type", "value": "example value"}],
+            "aggregation": "example aggregation",
+            "sort_order": "example sort_order",
+            "timeframe": "example timeframe",
+        }
+    ],
+}
 agent = MockAgent()
 
 
-def test_solve_all_tasks_no_tasks():
-    answer = solve_all_tasks({"tasks": []})
+def test_solve_all_no_tasks():
+    answer = solve_all({"questions": []})
 
     assert answer == no_tasks_response
 
 
-def test_solve_all_tasks_gets_final_answer(mocker):
+def test_solve_all_gets_final_answer(mocker):
     task_1_answer = "the answer is 42"
     mocker.patch("src.supervisors.supervisor.solve_task", return_value=("the_best_agent", task_1_answer))
 
-    answer = solve_all_tasks(task_dict)
-
-    assert answer == task_1_answer
+    scratchpad = solve_all(intent_json)
+    print(f"Scratchpad: {scratchpad}")
+    assert scratchpad[0]["result"] == task_1_answer
 
 
 def test_solve_task_first_attempt_solves(mocker):
