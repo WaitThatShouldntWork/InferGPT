ef48baa06985af8e090925f0dbbda1d8fa1af959
Harry <hbedford@scottlogic.com>
2024-06-17 16:48:27 +0100

    remove logs and notes

--- backend/src/agents/__init__.py
+++ backend/src/agents/__init__.py
@@ -10,16 +10,6 @@ from .answer_agent import AnswerAgent
 
 config = Config()
 
-"""
-    This module is being hit by certain tests, and for some reason the env variables in `test-backent.yml` are not being set
-    So, two options:
-        - get the env variables working in ci
-        - separate this modul (There is a ticket for this...)
-"""
-print("HELLO - inside agents init. INTENT_AGENT_LLM = ", config.intent_agent_llm)
-print("HELLO - inside agents init. MISTRAL_KEY = ", config.mistral_key)
-print("HELLO - inside agents init. ANSWER_AGENT_LLM = ", config.answer_agent_llm)
-print("HELLO - inside agents init. CONFIG = ", config)
 
 validator_agent = ValidatorAgent(get_llm(config.validator_agent_llm))
 intent_agent = IntentAgent(get_llm(config.intent_agent_llm))
