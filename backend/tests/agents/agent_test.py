c9184c977f7e6ea17dc7a6350ab27416153f51b9
Hélène Sauvé <hsauve@scottlogic.com>
2024-07-01 16:24:38 +0100

    Add model as arg in tests

--- backend/tests/agents/agent_test.py
+++ backend/tests/agents/agent_test.py
@@ -14,14 +14,14 @@ def test_agent_metadata_name():
 def test_agent_metadata_tools():
     assert MockAgent.tools == mock_tools
 
-
-mock_model = get_llm("mockllm")
-mock_agent_instance = MockAgent("mockllm")
+mock_model = "mockmodel"
+mock_llm = get_llm("mockllm")
+mock_agent_instance = MockAgent("mockllm", mock_model)
 
 
 def test_agent_invoke_uses_tool(mocker):
     mock_response = """{"tool_name": "Mock Tool A", "tool_parameters": { "input": "value for input" }, "reasoning": "Mock reasoning" }"""  # noqa: E501
-    mock_model.chat = mocker.MagicMock(return_value=mock_response)
+    mock_llm.chat = mocker.MagicMock(return_value=mock_response)
 
     response = mock_agent_instance.invoke("Mock task to solve")
 
@@ -30,7 +30,7 @@ def test_agent_invoke_uses_tool(mocker):
 
 def test_agent_invoke_with_no_tool(mocker):
     mock_response = """{"tool_name": "Undefined Tool", "tool_parameters": {}, "reasoning": "Mock reasoning"}"""
-    mock_model.chat = mocker.MagicMock(return_value=mock_response)
+    mock_llm.chat = mocker.MagicMock(return_value=mock_response)
 
     with raises(Exception) as error:
         mock_agent_instance.invoke("Mock task to solve")
@@ -43,7 +43,7 @@ def test_agent_invoke_no_appropriate_tool_for_task(mocker):
     mock_response = (
         """{"tool_name": "None", "tool_parameters": {}, "reasoning": "No tool was appropriate for the task"}"""
     )
-    mock_model.chat = mocker.MagicMock(return_value=mock_response)
+    mock_llm.chat = mocker.MagicMock(return_value=mock_response)
 
     with raises(Exception) as error:
         mock_agent_instance.invoke("Mock task to solve")
