05cae635b625b55472032fb80a7fe17fe17936d1
Hélène Sauvé <hsauve@scottlogic.com>
2024-07-17 11:23:10 +0100

    Create Developer agent + generate code tool

--- backend/src/agents/__init__.py
+++ backend/src/agents/__init__.py
@@ -29,11 +29,10 @@ def agent_details(agent) -> dict:
 
 
 def get_available_agents() -> List[Agent]:
-    return [
-        DatastoreAgent(config.datastore_agent_llm, config.datastore_agent_model),
-        WebAgent(config.web_agent_llm, config.web_agent_model),
-        ChartGeneratorAgent(config.chart_generator_llm, config.chart_generator_model),
-    ]
+    return [DatastoreAgent(config.datastore_agent_llm, config.datastore_agent_model),
+            WebAgent(config.web_agent_llm, config.web_agent_model),
+            ChartGeneratorAgent(config.chart_generator_llm, config.chart_generator_model),
+            ]
 
 
 def get_agent_details():
