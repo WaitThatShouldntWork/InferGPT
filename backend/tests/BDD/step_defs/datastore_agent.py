577b1067c80f0f893ab3c011e3e8302711590130
Svetoslav Anastasov <sanastasov13@gmail.com>
2024-07-18 16:27:03 +0100

    Initial test working with LangSmith

---
+++ backend/tests/BDD/step_defs/datastore_agent.py
@@ -0,0 +1,30 @@
+from pytest_bdd import scenario, given, when, then, parsers, scenarios
+from tests.BDD.test_utilities import (
+    send_prompt, 
+    app_healthcheck, 
+    correctness_evaluator, 
+    healthy_response
+)
+
+scenarios("../features/datastore_agent.feature")
+
+@given(parsers.parse("a transactional prompt to InferGPT"))
+def prepare_prompt():
+    response = app_healthcheck
+    #assert response.status_code == 200
+    assert response.json() == healthy_response
+
+@when(parsers.parse("I get the response"))
+def get_response():
+    pass
+
+@then(parsers.parse("the response to this '{prompt}' should match the {expected_amount}"))
+def check_response_includes_critical_info(prompt, expected_amount):
+    response = send_prompt(prompt)
+    result = correctness_evaluator.evaluate_strings( # type: ignore
+        input=prompt, # type: ignore
+        prediction=response.json(),
+        reference= expected_amount,
+    )
+    assert result["score"] == 1, "The bot response is not correct. \nReasoning: " + result["reasoning"]
+    print("Hello: ", result)
