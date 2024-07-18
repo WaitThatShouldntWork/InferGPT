b3dfa4585fb0c2a8a2d63431339b092391f808eb
Svetoslav Anastasov <sanastasov13@gmail.com>
2024-07-18 16:27:03 +0100

    Initial test working with LangSmith

--- backend/tests/BDD/test_utilities.py
+++ backend/tests/BDD/test_utilities.py
@@ -1,16 +1,24 @@
 from src.api import app
 from fastapi.testclient import TestClient
-from langchain.evaluation import EvaluatorType, StringEvaluator, load_evaluator
+from langchain.evaluation import load_evaluator
+from langchain.evaluation import EvaluatorType
 from langchain_openai.chat_models import ChatOpenAI
-from langchain.agents import AgentExecutor
+
 
 START_ENDPOINT_URL = "/chat?utterance={utterance}"
 CONVERSATION_ENDPOINT_URL = "/chat?utterance={utterance}"
 HEALTHCHECK_ENDPOINT_URL = "/health"
 health_prefix = "InferGPT healthcheck: "
 healthy_response = health_prefix + "backend is healthy. Neo4J is healthy."
+HEALTHCHECK_ENDPOINT_URL = "/health"
+health_prefix = "InferGPT healthcheck: "
+healthy_response = health_prefix + "backend is healthy. Neo4J is healthy."
 
 client = TestClient(app)
+
+def app_healthcheck():
+    healthcheck_response = client.get(HEALTHCHECK_ENDPOINT_URL)
+    return healthcheck_response
 
 def app_healthcheck():
     healthcheck_response = client.get(HEALTHCHECK_ENDPOINT_URL)
@@ -24,13 +32,12 @@ def send_prompt(prompt: str):
 ##Evaluation LLM
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, max_retries=2)
 
-correctness_evaluator: StringEvaluator = load_evaluator( # type: ignore
-    EvaluatorType.LABELED_CRITERIA, criteria="correctness", llm=llm)
+correctness_evaluator = load_evaluator(EvaluatorType.LABELED_CRITERIA, criteria="correctness", llm=llm)
 
 confidence_criterion = {
     "confidence": "Does the bot seem confident that it replied to the question and gave the correct answer?"
 }
 
-confidence_evaluator: StringEvaluator = load_evaluator(  # type: ignore
+confidence_evaluator: load_evaluator(  # type: ignore
     EvaluatorType.CRITERIA, criteria=confidence_criterion, llm=llm
 )
