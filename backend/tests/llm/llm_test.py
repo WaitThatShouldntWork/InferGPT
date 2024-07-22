fe5f6f5ef4317310e72db33bb7116cd9b4a454fa
Seth Wood <swood@scottlogic.com>
2024-07-22 16:36:08 +0100

    Fix IncompatibleMethodOverride errors

--- backend/tests/llm/llm_test.py
+++ backend/tests/llm/llm_test.py
@@ -9,7 +9,7 @@ def test_chat_exists():
 
 
 def test_chat_returns_string():
-    response = model.chat("system prompt", "user prompt")
+    response = model.chat("model", "system prompt", "user prompt")
 
     assert isinstance(response, str)
 
@@ -17,7 +17,7 @@ def test_chat_returns_string():
 def test_chat_increments_counter(mocker):
     counter_mock = mocker.patch("src.llm.count_calls.counter")
 
-    model.chat("system prompt", "user prompt")
+    model.chat("model", "system prompt", "user prompt")
 
     assert counter_mock.increment.call_count == 1
 
@@ -27,7 +27,7 @@ def test_chat_multi_model(mocker):
     counter_mock = mocker.patch("src.llm.count_calls.counter", counter)
     model_2 = MockLLM()
 
-    model.chat("system prompt", "user prompt")
-    model_2.chat("system prompt", "user prompt")
+    model.chat("model", "system prompt", "user prompt")
+    model_2.chat("model", "system prompt", "user prompt")
 
     assert counter_mock.count == 2
